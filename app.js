3761f106188670120b14ad4d68a1b8de5ab5b26c
Shubham SHARMA <shubham@sharma.fr>
2014-10-11 01:06:14 +0200

    Added twitter stream api

--- app.js
+++ app.js
@@ -3,17 +3,33 @@ var http = require('http').Server(app);
 var http_for_time = require('http');
 var io = require('socket.io')(http);
 
+var credentials = require('./credentials.js');
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+var Twitter_stream = require('user-stream');
+var stream = new Twitter_stream(credentials.twitter);
+var params = {
+    with: 'shub_s' // The id of your competitor :)
+}
+
+// create the twitter stream
+stream.stream(params);
+
+//listen stream data
+stream.on('data', function(json) {
+
+	//here traking when someone followes hi,
+	if ((typeof json.event != 'undefined') && (json.event == 'follow')) {
+		console.log(params.with + " was followed by " + json.source.name + " he now has " + json.target.followers_count + "followers");
+	};
 });
 
-setInterval(function(){
-	console.log("hello");
+stream.on('connected', function(json) {
+  console.log("Twitter user connected");
+});
 
-	io.emit('update', { msg: 'Hey dude'});
-
-}, 2000);
+app.get('/', function(req, res){
+  res.sendfile('front/index.html');
+});
 
 http.listen(80, function(){
   console.log('listening on http://localhost:80');
