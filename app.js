6489380791f761284afb4f6e6bfed2ffc47d02b3
Shubham SHARMA <shubham@sharma.fr>
2014-10-15 12:00:50 +0200

    running on server

--- app.js
+++ app.js
@@ -84,6 +84,8 @@ app.get('/', function(req, res){
   res.sendfile('front/index.html');
 });
 
-http.listen(80, function(){
-  console.log('listening on http://localhost:80');
+var port = Number(process.env.PORT || 5000);
+http.listen(port, function(){
+    console.log('listening on http://localhost:'+port);
 });
+
